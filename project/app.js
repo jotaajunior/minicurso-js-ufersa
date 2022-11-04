6b6d85312ec2448ca172079e9148a8c1c51f95c6
Jota Júnior <jjunior@protonmail.ch>
2022-11-03 22:31:42 -0300

    feat: configure cors

--- project/app.js
+++ project/app.js
@@ -1,15 +1,17 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
 import * as candidateRoutes from './routes/candidate.routes.js';
 
 const app = fastify({
   logger: true,
 });
 
+app.register(cors);
 candidateRoutes.configRoutes(app);
 
 async function bootstrap() {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: 8080 });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
